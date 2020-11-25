b97305a083ee279d6078cedcd0be52a4d5f8742a
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2020-11-25 14:18:44 -0500

    all project

---
+++ src/AcklenTest.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcklenTest.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/AcklenTest.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AcklenTest.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
